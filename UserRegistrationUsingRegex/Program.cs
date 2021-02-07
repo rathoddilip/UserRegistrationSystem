ae41d49b225f2cfe0672c167cc575210647efee4
diliprathod32 <diliprathod32@gmail.com>
2021-02-07 23:08:34 +0530

    [Dilip] FistNameValidation

--- UserRegistrationUsingRegex/Program.cs
+++ UserRegistrationUsingRegex/Program.cs
@@ -1,15 +1,31 @@
-﻿using System;
+﻿
+using System;
 using System.Text.RegularExpressions;
 
 namespace UserRegistrationUsingRegex
 {
+    public class UserRegex
+    {
+       
+        internal void FirstNameRegex(string Firstname)
+        {
+            Regex firstNameregex = new Regex("^[A-Z][a-zA-Z]{2,}$");
+            Console.WriteLine("Firstname: " + Firstname);
+
+            if (firstNameregex.IsMatch(Firstname))
+                Console.WriteLine("Valid First name");
+            else
+                Console.WriteLine("Invalid first name");
+        }
+    }
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to user registration system");
+            UserRegex userRegex = new UserRegex();
+            string Firstname = "Dilip";
+            userRegex.FirstNameRegex(Firstname);
         }
-
-       
     }
 }
